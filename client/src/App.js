1704088cdd3febbcd10e81d560c08e2c31fda225
unknown <bmulaw@bu.edu>
2021-12-24 21:40:57 -0800

    initial push

---
+++ client/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        Hello World, this is Bernard
+      </header>
+    </div>
+  );
+}
+
+export default App;
