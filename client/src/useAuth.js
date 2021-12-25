f010dd3150c5b3a5e9027e5062677643a723ec68
unknown <bmulaw@bu.edu>
2021-12-25 00:30:39 -0800

    refresh token timer

--- client/src/useAuth.js
+++ client/src/useAuth.js
@@ -11,7 +11,32 @@ export default function useAuth(code) {
             code,
         })
         .then(res => {
-            console.log(res.data);
+            setAccessToken(res.data.access_token);
+            setRefreshToken(res.data.refresh_token);
+            setExpiresIn(res.data.expires_in);
+            window.history.pushState({}, null, '/')
+        })
+        .catch(() => {
+            window.location = "/";
         })
     }, [code])
+
+    useEffect(() => {
+        if (!refreshToken || !expiresIn) return
+        const interval = setInterval(() => {
+        axios.post('http://localhost:3001/refresh', {
+            refreshToken,
+        })
+        .then(res => {
+            setAccessToken(res.data.access_token);
+            setExpiresIn(res.data.expires_in);
+        })
+        .catch(() => {
+            window.location = "/";
+        })
+    }, (expiresIn - 60) * 1000)
+
+    return () => clearInterval(interval);
+    }, [refreshToken, expiresIn]);
+    return accessToken;
 }
