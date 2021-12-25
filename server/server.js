b2d47ad5f9d221051ef64d189a63a131ec75491f
unknown <bmulaw@bu.edu>
2021-12-25 00:28:09 -0800

    cors an body-parser, refresh token

--- server/server.js
+++ server/server.js
@@ -1,7 +1,31 @@
 const express = require('express');
 const SpotifyWebApi = require('spotify-web-api-node');
+const bodyParser = require('body-parser');
+const cors = require('cors')
 
 const app = express();
+app.use(cors());
+app.use(bodyParser.json());
+
+app.post('/refresh', (req, res) => {
+    const refreshToken = req.body.refreshToken;
+    const spotifyApi = new SpotifyWebApi({
+        redirectUri: "http://localhost:3000",
+        clientId: "45c8cca7568346c09d2ae3e15488221c",
+        clientSecret: "d6fa9189dfbc4a4d9a589bad895830f0",
+        refreshToken: ""
+    })
+
+    spotifyApi.refreshAccessToken()
+    .then((data) => {
+            res.json({
+                accessToken: data.body.accessToken,
+                expiresIn: data.body.expiresIn
+            })
+    }).catch(() => {
+        res.sendStatus(400);
+    })
+})
 
 app.post('/login', (req, res) => {
     const code = req.body.code;
