38c882c5ae73968ecaa0e45f25fa691ff0972e4a
unknown <bmulaw@bu.edu>
2021-12-25 21:09:23 -0800

    playing song will get song's id from genuis

--- client/src/Samples.js
+++ client/src/Samples.js
@@ -1,14 +1,13 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import Player from './Player'
 
 export default function Samples({ playingTrack }) {
     const [song, setSong] = useState([]);
+    const [songId, setSongId] = useState();
     var axios = require("axios").default;
 
-    var music = playingTrack['title'] + " " + playingTrack['artist']
-    if (music != 'undefined undefined') {
-        console.log('music', music); 
+    const callGenuisApi = () => {
         var options = {
             method: 'GET',
             url: 'https://genius.p.rapidapi.com/search',
@@ -16,33 +15,50 @@ export default function Samples({ playingTrack }) {
             headers: {
                 'x-rapidapi-host': 'genius.p.rapidapi.com',
                 'x-rapidapi-key': 'dfcaddd723msh6b216a6f6b60baep1fb122jsn29391c842471'
-            }}
-    };
-
-    const getSongID = (data)  =>{
-        var id = 0
-        for (var i = 0; i < data.length; i++) {
-            if(data[i].result.title == playingTrack['title'] 
-            && playingTrack['artist'] in data[i].result.artist_names) {
-                id = data[i].result.id
-            }
-        }
-        console.log(id, 'songid')
-        return id;
+        }}
+        return options;
     }
 
-    axios.request(options)
-        .then(res => {
-            console.log('id', getSongID(res.data.response.hits))
-            console.log(res.data.response.hits)
-            setSong(res.data.response.hits[0]);
-        })
-        .catch(function (error) {
-            console.error(error);
-        });
+    const getSongID = (data)  =>{
+        var index = 0;
+        for (var i = 0; i < data.length; i++) {
+            if(data[i].result.title.includes(songTitle)
+            && data[i].result.artist_names.includes(playingTrack['artist'])) {
+                index = i;
+                break;
+            }
+        }
+        const id = data[index].result.id
+        return [id, index];
+    }
+    if (playingTrack['title'] != undefined) {
+        var songTitle = playingTrack['title'].replace(/\([^()]*\)/g, '')
+        var music = songTitle + " " + playingTrack['artist']
+    }
+    useEffect(() => {
+        if (music != 'undefined undefined') {
+            const options = callGenuisApi();
+            axios.request(options)
+            .then(res => {
+                const [id, index] = getSongID(res.data.response.hits);
+                setSong(res.data.response.hits[index].result);
+                console.log(song);
+                if (id !== 0) {
+                    setSongId(id);
+                } else {
+                    console.log('50', song.id)
+                    setSongId(song.id)
+                }
+            })
+            .catch(function (error) {
+                console.error(error);
+            });
+        };
+    }, [playingTrack]); 
+    
     return (
         <div>
-            {song}
+            {songId}
         </div>
     )
 }
