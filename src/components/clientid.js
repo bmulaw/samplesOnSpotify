bbba0ee9b5694f32ee6a2f33dde7696c72139457
unknown <bmulaw@bu.edu>
2022-02-08 01:33:04 -0500

    config

---
+++ src/components/clientid.js
@@ -0,0 +1,3 @@
+export default function clientid() {
+    return "45c8cca7568346c09d2ae3e15488221c"
+}
---
+++ src/components/config.js
@@ -0,0 +1,4 @@
+export default function config() {
+    const API_KEY = '2aa42bb1a5msh9ffad36481a9b7ep109288jsn91c47e343249'
+    return API_KEY
+} 
